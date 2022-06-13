3d1ff7e76832b0c5f8f0a2e3543f2aea6d75ac7f
Mohamed Mousa <mohamedamousa00@gmail.com>
2022-06-13 16:18:38 +0200

    add  sample document

---
+++ DotnetMongoDB.API/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using DotnetMongoDB.API.Abstraction;
+using DotnetMongoDB.API.Settings;
+using System;
+
+namespace DotnetMongoDB.API.Models
+{
+    [BsonCollection("people")]
+    public class Person : Document
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
