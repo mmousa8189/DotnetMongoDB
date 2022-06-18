5a1acf899a0b3c97cc83fd96f628913ebe86cf14
Mohamed Mousa <mohamedamousa00@gmail.com>
2022-06-18 22:27:32 +0200

    add AddFluentValidation

--- DotnetMongoDB.API/Controllers/SampleController.cs
+++ DotnetMongoDB.API/Controllers/SampleController.cs
@@ -1,5 +1,6 @@
 ﻿using DotnetMongoDB.API.Models;
 using DotnetMongoDB.API.Repository;
+using DotnetMongoDB.API.Validators;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -37,6 +38,38 @@ namespace DotnetMongoDB.API.Controllers
                 projection => projection.FirstName
             );
             return people;
+        }
+
+        [HttpPost("addCustomer")]
+        public IActionResult Add(CustomerModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+                // re-render the view when validation failed.
+                //return View(model);
+            }
+
+            //TODO: Save the data to the database, or some other logic.
+
+            return Ok();
+        }
+        // Manual validation
+        [HttpPut("updateCustomer")]
+        public IActionResult Update(CustomerModel model)
+        {
+            CustomerValidator validator = new CustomerValidator();
+            var validationResult = validator.Validate(model);
+
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
+            //TODO: Save the data to the database, or some other logic.
+
+            return Ok();
         }
     }
 }
